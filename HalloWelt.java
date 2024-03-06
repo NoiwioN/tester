7a094457f0abf5a893a4a5714271649be9d44916
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-03-06 10:45:05 +0100

    WTF

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,7 +1,5 @@
 public class HalloWelt {
-    private void halloWelt(){
-        System.out.println("Hallo Welt??");
-    }
+
     public static void main(String[] args) {
         System.out.println("Hallo Welt TEst");
     }
