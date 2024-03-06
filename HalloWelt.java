2959e19624d07727d38d7ba2e3d497fa4d583fc8
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-03-06 10:48:51 +0100

    Funktion

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,5 +1,7 @@
 public class HalloWelt {
-
+    private void halloWelt(){
+        System.out.println("Hallo Welt");
+    }
     public static void main(String[] args) {
         System.out.println("Hallo Welt TEst");
     }
